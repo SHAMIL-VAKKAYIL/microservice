e1033e3a62ce28846177563115f956134ba28c00
shamil <muhammedshamil470@gmail.com>
2025-07-23 07:40:56 +0530

    user token implimented

---
+++ auth-service/utils/Token.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+
+exports.userToken = (id, res) => {
+    const token = jwt.sign({ id }, process.env.JWT_SCERATE, { expiresIn: '7d' })
+
+    res.cookie('user', token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        sameSite: 'strict',
+        httpOnly: false,
+        secure: process.env.NODE_ENV === 'production',
+    })
+    return token
+
+}
+// JWT_SCERATE=DKFJGHDSIKFGHSIF
+// NODE_ENV=development
+// NODE_ENV=production
