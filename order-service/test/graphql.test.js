e01c7ded122e76b9a504fec05c6f0e241c9b5c28
SHAMIL-VAKKAYIL <muhammedshamil470@gmail.com>
2025-08-13 12:54:48 +0530

    complete  graphql query testing code

--- order-service/test/graphql.test.js
+++ order-service/test/graphql.test.js
@@ -7,6 +7,63 @@ beforeAll(async () => {
     app = await startServer()
 })
 
+
+// describe('orders ',()=>{
+//     it('should get all orders', async () => {
+//         const query = `
+//         query {
+//             orders {
+//                 id
+//                 products 
+//                 totalPrice
+//                 status
+//         }`
+//         const res = await request(app).post('/graphql').send({ query }).set('Content-Type', 'application/json')
+//         console.log(res.body.data.orders);
+//         expect(res.statusCode).toBe(200);
+//         expect(res.body.data.orders).toBeDefined();
+//     })
+// })
+
+
+// describe('my orders', () => {
+//     it('should get my orders', async () => {
+//         const query = `
+//         query {
+//             myOders {
+//                 id
+//                 products 
+//                 totalPrice
+//                 status
+//             }
+//         }`
+//         const res = await request(app).post('/graphql').send({ query }).set('Content-Type', 'application/json')
+//         console.log(res.body.data.myOders);
+//         expect(res.statusCode).toBe(200);
+//         expect(res.body.data.myOders).toBeDefined();
+//     })
+// })
+
+
+// desctribe('single order', () => {
+//     it('should get single order', async () => {
+//         const query = `
+//         query {
+//             singleOrder(id: "order_123") {
+//                 id
+//                 products 
+//                 totalPrice
+//                 status
+//             }
+//         }`
+
+//         const res = await request(app).post('/graphql').send({ query }).set('Content-Type', 'application/json')
+//         console.log(res.body.data.singleOrder);
+//         expect(res.statusCode).toBe(200);
+//         expect(res.body.data.singleOrder).toBeDefined();
+//     })
+// })
+
 describe('order graphql', () => {
     it('should create an order', async () => {
         const query = `
